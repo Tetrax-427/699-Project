c073ce29c6c1de15c44f696a436b0444ab2b315e
Tetrax-427 <84516712+Tetrax-427@users.noreply.github.com>
2022-11-04 23:55:50 +0530

    Add files via upload

--- 699/main.py
+++ 699/main.py
@@ -1,29 +1,74 @@
 import mimetypes
-from flask import Flask,send_file,render_template,request
+from flask import Flask, flash, redirect,send_file,render_template,request
 import io
 import base64
+from werkzeug.utils import secure_filename
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import os
+import shutil
 
-print("Editing by somil")
-data=pd.read_excel("data.xlsx")
-X=[1,2,3,4,5,6,7,8,9]
-Y=[12,8,4,2,1,2,4,8,12]
 
 app=Flask(__name__,template_folder='template')
+file_names=[]
+app.secret_key = "secret key"
+path = os.getcwd()
+
+UPLOAD_FOLDER = os.path.join(path, 'uploads')
+shutil.rmtree(UPLOAD_FOLDER, ignore_errors=False, onerror=None)
+
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Allowed extension you can set your own
+ALLOWED_EXTENSIONS = set(['xlsx','csv'])
+
+data=pd.read_excel("data.xlsx")
+X=[1,2,3,4,5,6,7,8,9]
+Y=[6,8,4,2,1,2,4,8,6]
+
+
 x_size=6
 y_size=6
 x_col="X-Label"
 y_col="Y-Label"
 plot_type=1
+print("Fuck")
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def home():
     return render_template('form.html')
 
-@app.route('/', methods =["GET", "POST"])
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    global file_names
+    if request.method == 'POST':
+
+        if 'files[]' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        files = request.files.getlist('files[]')
+
+        for file in files:
+            if file and allowed_file(file.filename):
+                
+                filename = secure_filename(file.filename)
+                file_names.append(filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        flash('File(s) successfully uploaded')
+        return render_template('form.html')
+    
+
+@app.route('/form_data', methods =["GET", "POST"])
 def form_data():
     if request.method == "POST":
         
@@ -34,7 +79,8 @@ def form_data():
         global y_col
         
         global plot_type
-
+        global file_names
+        print("Fucku")
         # getting input with name = x_col in HTML form
         #data=request.form.get("FILE")
         x_col= request.form.get("x_col")
@@ -44,8 +90,11 @@ def form_data():
         plot_type=request.form.get("plot_type")
         
         
-        X=data[x_col].to_list()
-        Y=data[y_col].to_list()
+        uploaded_file = os.path.join(UPLOAD_FOLDER, file_names[0])
+        df=pd.read_csv(uploaded_file)
+        print(df.head())
+        X=df[x_col].to_list()
+        Y=df[y_col].to_list()
 
         x_size=6
         y_size=6
@@ -66,6 +115,7 @@ def visualize():
         fig.savefig(img)
         img.seek(0)
         return send_file(img,mimetype='img/png')
+    
     elif plot_type=="2":
         fig,ax= plt.subplots(figsize=(x_size,y_size))
         ax=sns.set_style(style="darkgrid")
@@ -77,6 +127,7 @@ def visualize():
         fig.savefig(img)
         img.seek(0)
         return send_file(img,mimetype='img/png')
+    
     elif plot_type=="3":
         fig,ax= plt.subplots(figsize=(x_size,y_size))
         ax=sns.set_style(style="darkgrid")
